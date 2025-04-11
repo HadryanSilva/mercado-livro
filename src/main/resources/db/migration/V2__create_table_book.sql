10757a3798107521813077b7e3bedb595c719c77
Hadryan Silva <hadryan2001@gmail.com>
2025-04-11 01:54:08 -0300

    chore: add create_table_book script + fix create_table_customer syntax

---
+++ src/main/resources/db/migration/V2__create_table_book.sql
@@ -0,0 +1,8 @@
+CREATE TABLE book(
+     id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
+     name VARCHAR(255) NOT NULL,
+     price DECIMAL(10, 2) NOT NULL,
+     customer_id BIGINT NOT NULL,
+     PRIMARY KEY (id),
+     FOREIGN KEY (customer_id) REFERENCES customer(id)
+);
