8d8fc778bcbadf64244881083759bcc990bef766
Hadryan Silva <hadryan2001@gmail.com>
2025-04-12 02:04:57 -0300

    fix: change exception handler return to ResponseEntity

--- src/main/kotlin/com/mercadolivro/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/mercadolivro/exception/GlobalExceptionHandler.kt
@@ -2,31 +2,36 @@ package com.mercadolivro.exception
 
 import com.mercadolivro.controller.response.ErrorResponse
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.context.request.WebRequest
 
-@RestControllerAdvice
+@ControllerAdvice
 class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException::class)
-    fun handleNotFoundException(exception: NotFoundException, request: WebRequest): ErrorResponse? {
-        return ErrorResponse(
+    fun handleNotFoundException(exception: NotFoundException, request: WebRequest): ResponseEntity<ErrorResponse> {
+        val response = ErrorResponse(
             httpCode = HttpStatus.NOT_FOUND.value(),
             message = exception.message,
             internalCode = "ML-001",
             errors = null
         )
+
+        return ResponseEntity(response, HttpStatus.NOT_FOUND)
     }
 
     @ExceptionHandler(NullPointerException::class)
-    fun handleNullPointerException(exception: NullPointerException, request: WebRequest): ErrorResponse? {
-        return ErrorResponse(
+    fun handleNullPointerException(exception: NullPointerException, request: WebRequest): ResponseEntity<ErrorResponse> {
+        val response =  ErrorResponse(
             httpCode = HttpStatus.INTERNAL_SERVER_ERROR.value(),
             message = exception.message ?: "Null pointer exception",
             internalCode = "ML-002",
             errors = null
         )
+
+        return ResponseEntity(response, HttpStatus.INTERNAL_SERVER_ERROR)
     }
 
 }
