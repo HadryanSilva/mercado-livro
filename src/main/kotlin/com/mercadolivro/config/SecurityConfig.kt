d412edd8e65769a1dfddf383fd0c25d407b12dc7
Hadryan Silva <hadryan2001@gmail.com>
2025-04-13 01:40:31 -0300

    feat: Add simple security filter chain

--- src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
+++ src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
@@ -2,11 +2,34 @@ package com.mercadolivro.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfig {
+
+    private val PUBLIC_POST_ENDPOINTS = arrayOf(
+        "/api/v1/customer"
+    )
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .authorizeHttpRequests {
+                it.requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                it.requestMatchers(HttpMethod.POST, *PUBLIC_POST_ENDPOINTS).permitAll()
+                it.anyRequest().authenticated()
+            }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+        return http.build()
+    }
 
     @Bean
     fun passwordEncoder(): PasswordEncoder {
